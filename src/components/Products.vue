d27fbe67b137ea4955a239b158a49879544c939a
Baranuel <samuel.baran98@gmail.com>
2022-01-04 13:04:26 +0100

    Store

---
+++ src/components/Products.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class=" bg-red-100 w-9/12 h-96 grid grid-cols-4 grid-rows-2 gap-2  mx-auto">
+      <div v-for="headline in headlines" :key="headline.id" :src="  headline.headline" class=" h-full w-full  bg-gray-400">
+          <h1>{{headline.id}}</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+import headlines from '../data/products'
+export default {
+    name:'Products',
+    components:{},
+    data(){
+        return{
+            headlines
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/data/products.js
@@ -0,0 +1,15 @@
+export default [
+
+    {
+        id:1,
+        headline:'girl-heart.jpg',
+    },
+    {
+        id:2,
+        headline:'girl-front.jpg',
+    },
+    {
+        id:3,
+        headline:'girl-baw.jpg',
+    },
+]
