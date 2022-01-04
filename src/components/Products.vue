c4064b21bb949b7a2a80fb0f0d4e26c9a3a40b75
Baranuel <samuel.baran98@gmail.com>
2022-01-04 13:42:32 +0100

    getting the images from js file

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class=" bg-red-100 w-9/12 h-96 grid grid-cols-4 grid-rows-2 gap-2  mx-auto">
-      <div v-for="headline in headlines" :key="headline.id" :src="  headline.headline" class=" h-full w-full  bg-gray-400">
+<div class=" bg-red-100 w-9/12 flex justify-center mx-auto">
+
+  <div class="grid w-8/12 grid-cols-4 grid-rows-auto gap-3 gap-y-6 m-6" >
+      <headImage v-for="headline in headlines" :key="headline.id" :src="'./assets/' + headline.headline" class=" h-44 w-44 object-cover bg-gray-400 justify-self-center">
           <h1>{{headline.id}}</h1>
-      </div>
+      </headImage>
   </div>
+</div>
 </template>
 
 <script>
 import headlines from '../data/products'
+import headImage from './headImage.vue'
 export default {
     name:'Products',
-    components:{},
+    components:{
+        headImage
+    },
     data(){
         return{
             headlines
