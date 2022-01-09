6c1d8e23bb510f7510373f2b176cb84cb8ea42eb
lars-av <74174119+lars-av@users.noreply.github.com>
2022-01-09 17:40:09 +0100

    Did the thing

---
+++ src/components/IndividualStaffMember.vue
@@ -0,0 +1,24 @@
+<template>
+     <div class="box bg-white rounded-b-sm">
+      <div class="imageContainerStaff">
+        <img class="w-full h-60 object-cover object-center rounded-t-sm" :src="require('/public/assets/' + filename)" :alt="(name)"/>
+      </div>
+      <h4 class="headline-process text-left text-black my-4 pt-2 px-6">{{ name }}</h4>
+      <p class="paragraph-text text-lg px-6 pt-1 pb-4">
+      <slot>
+          
+      </slot>
+      </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Staff',
+    props: ['name', 'filename']
+}
+</script>
+
+<style>
+
+</style>
